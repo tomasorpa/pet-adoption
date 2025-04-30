1d1a8e9b30b0be061cd59b4d2592f4f422aa87ce
tomasorpa <tomasorpa@gmail.com>
2025-04-30 19:04:40 -0400

    First configuration

--- netlify/functions/pets.js
+++ netlify/functions/pets.js
@@ -1,7 +1,21 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const handler = async () => {
+  const client = new MongoClient(process.env.DB_CNN);
+  await client.connect();
+  const pets = await client
+    .db("petAdoptionCenter")
+    .collection("pets")
+    .find()
+    .toArray();
+  client.close();
   return {
     statusCode: 200,
-    body: "tomas".toUpperCase(),
+    headers: {
+      "Content-Type": "appplication/json",
+      "Access-Control-Allow-origin": "*",
+    },
+    body: JSON.stringify(pets),
   };
 };
 
